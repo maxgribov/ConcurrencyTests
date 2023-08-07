39436f13727ae984cc5ffb848bf6ce2627b90998
Max Gribov <gribov.max@gmail.com>
2023-08-07 12:27:25 +0300

    basic tests implemented

--- ConcurrencyTestsTests/ConcurrencyTestsTests.swift
+++ ConcurrencyTestsTests/ConcurrencyTestsTests.swift
@@ -8,29 +8,100 @@
 import XCTest
 @testable import ConcurrencyTests
 
+struct FeedItem: Equatable {
+    
+    let id: UUID
+    let name: String
+}
+
+protocol FeedClient {
+    
+    func getItems(from: URL) async throws -> [FeedItem]
+}
+
+final class FeedItemsLoader {
+    
+    private let client: any FeedClient
+    
+    init(client: any FeedClient) {
+        
+        self.client = client
+    }
+    
+    static let feedURL = URL(string: "https://some-url.com")!
+    
+    func loadItems() async throws -> [FeedItem] {
+        
+        try await client.getItems(from: FeedItemsLoader.feedURL)
+    }
+}
+
 final class ConcurrencyTestsTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func test_loadItems_returnsResultIfSUTInstanceNotDeinited() async throws {
+        
+        swift_task_enqueueGlobal_hook = { job, _ in
+            MainActor.shared.enqueue(job)
         }
+        
+        let client = FeedClientSpy()
+        let sut = FeedItemsLoader(client: client)
+        let expectedResult = [FeedItem(id: UUID(), name: "Test")]
+        
+        let task = Task { [weak sut] in
+            
+            try await sut?.loadItems()
+        }
+        
+        await Task.yield() //suspend current task for a little bit, to allow other async tasks do their job
+        client.response(with: expectedResult)
+        let result = try await task.value
+                
+        XCTAssertEqual(result, expectedResult)
     }
+    
+    func test_loadItems_returnsNilAfterSUTInstanceDeinited() async throws {
+        
+        swift_task_enqueueGlobal_hook = { job, _ in
+            MainActor.shared.enqueue(job)
+        }
+        
+        let client = FeedClientSpy()
+        var sut: FeedItemsLoader? = FeedItemsLoader(client: client)
+        
+        let task = Task { [weak sut] in
+            
+            try await sut?.loadItems()
+        }
+        
+        sut = nil
+        await Task.yield() //suspend current task for a little bit, to allow other async tasks do their job
+        client.response(with: [.init(id: UUID(), name: "Test")])
+        let result = try await task.value
+        
+        XCTAssertNil(result)
+    }
+}
 
+private final class FeedClientSpy: FeedClient {
+    
+    private let responseContinuation: AsyncStream<[FeedItem]>.Continuation
+    private let responseStream: AsyncStream<[FeedItem]>
+    
+    init() {
+        
+        var responseContinuation: AsyncStream<[FeedItem]>.Continuation!
+        self.responseStream = AsyncStream { responseContinuation = $0 }
+        self.responseContinuation = responseContinuation
+    }
+    
+    func getItems(from: URL) async throws -> [FeedItem] {
+        
+        await responseStream.first(where: { _ in true })!
+    }
+    
+    func response(with items: [FeedItem]) {
+        
+        responseContinuation.yield(items)
+    }
 }

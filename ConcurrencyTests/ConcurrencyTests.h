7e6eac8c828c79a5553124390eae8ef3a94eb054
Max Gribov <gribov.max@gmail.com>
2023-08-07 11:27:56 +0300

    Initial Commit

---
+++ ConcurrencyTests/ConcurrencyTests.h
@@ -0,0 +1,18 @@
+//
+//  ConcurrencyTests.h
+//  ConcurrencyTests
+//
+//  Created by Max Gribov on 07.08.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ConcurrencyTests.
+FOUNDATION_EXPORT double ConcurrencyTestsVersionNumber;
+
+//! Project version string for ConcurrencyTests.
+FOUNDATION_EXPORT const unsigned char ConcurrencyTestsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConcurrencyTests/PublicHeader.h>
+
+
